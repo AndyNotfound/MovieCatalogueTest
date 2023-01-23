9bb49d7e224335a526db7669f46bee5e195d494c
AndyNotfound <Andy.Notfound@gmail.com>
2023-01-23 08:58:56 -0500

    Add: Automation test for liking and unliking movie, fix some bugs too

--- src/scripts/data/favorite-movie-idb.js
+++ src/scripts/data/favorite-movie-idb.js
@@ -1,22 +1,28 @@
-import { openDB } from 'idb';
-import CONFIG from '../globals/config';
+import { openDB } from "idb";
+import CONFIG from "../globals/config";
 
 const { DATABASE_NAME, DATABASE_VERSION, OBJECT_STORE_NAME } = CONFIG;
 
 const dbPromise = openDB(DATABASE_NAME, DATABASE_VERSION, {
   upgrade(database) {
-    database.createObjectStore(OBJECT_STORE_NAME, { keyPath: 'id' });
+    database.createObjectStore(OBJECT_STORE_NAME, { keyPath: "id" });
   },
 });
 
 const FavoriteMovieIdb = {
   async getMovie(id) {
+    if (!id) {
+      return;
+    }
     return (await dbPromise).get(OBJECT_STORE_NAME, id);
   },
   async getAllMovies() {
     return (await dbPromise).getAll(OBJECT_STORE_NAME);
   },
   async putMovie(movie) {
+    if (!movie.hasOwnProperty("id")) {
+      return;
+    }
     return (await dbPromise).put(OBJECT_STORE_NAME, movie);
   },
   async deleteMovie(id) {
